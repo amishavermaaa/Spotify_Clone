2759171f65262e69e40b3d7d5597546e30d4f79a
Amisha Verma <133965778+amishavermaaa@users.noreply.github.com>
2024-05-17 02:21:56 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:classico/root_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+
+  ));
+}
